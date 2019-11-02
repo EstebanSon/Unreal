ce9517a9338167d3ce6a14369a396be202b41b24
Esteban21 <isomorphism1126@gmail.com>
2019-11-02 21:04:07 +0900

    BatteryCollector 프로젝트 Commit

---
+++ BatteryCollector/Intermediate/Build/Win64/UE4Editor/Development/BatteryCollector/PCH.BatteryCollector.cpp
@@ -0,0 +1,1 @@
+#include "D:/Unreal/BatteryCollector/Intermediate/Build/Win64/UE4Editor/Development/BatteryCollector/PCH.BatteryCollector.h"
---
+++ BatteryCollector/Source/BatteryCollector/SpawnVolume.h
@@ -0,0 +1,65 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/Actor.h"
+#include "SpawnVolume.generated.h"
+
+UCLASS()
+class BATTERYCOLLECTOR_API ASpawnVolume : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ASpawnVolume();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	/** Returns to WhereToSpawn subobject */
+	FORCEINLINE class UBoxComponent* GetWhereToSpawn() const { return WhereToSpawn;  }
+
+	/** Find a random point within the BoxComponent */
+	UFUNCTION(BlueprintPure, Category = "Spawning")
+	FVector GetRandomPointVolume();
+
+	/** This function toggles whether or not the spawn volume spawns pickups */
+	UFUNCTION(BlueprintCallable, Category = "Spawning")
+	void SetSpawningActive(bool bShouldSpawn);
+
+protected:
+	/** The pickup to spawn */
+	UPROPERTY(EditAnywhere, Category = "Spawning")
+	TSubclassOf<class APickup> WhatToSpawn;
+
+	FTimerHandle SpawnTimer;
+
+	/** Minimum spawn delay */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, category = "Spawning")
+	float SpawnDelayRangeLow;
+	
+	/** Maximum spawn delay */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, category = "Spawning")
+	float SpawnDelayRangeHigh;
+	
+
+
+private:
+	/** Box Component to specify where pickups should be spawned */
+	UPROPERTY(VisibleAnyWhere, BlueprintReadOnly, Category = "Spawning", meta = (AllowPrivateAccess = "true"))
+	class UBoxComponent* WhereToSpawn;
+	
+	/** Handle spawning a new pickup */
+	void SpawnPickup();
+
+	/** The current spawn delay */
+	float SpawnDelay;
+	
+	
+};
